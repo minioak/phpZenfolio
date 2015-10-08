35f28d820dd175dec5860f78e29999a09a3875d7
John Mitchell <jfm@minioak.com>
2015-10-08 20:33:28 +0100

    Update phpZenfolio.php

--- phpZenfolio.php
+++ phpZenfolio.php
@@ -1134,8 +1134,8 @@ class PhpZenfolioCurlRequestProcessor implements PhpZenfolioRequestProcessor
 			CURLOPT_BUFFERSIZE		=> $config['buffer_size'],
 			CURLOPT_HTTPHEADER		=> $merged_headers,
 			CURLOPT_RETURNTRANSFER	=> TRUE,
-            CURLOPT_SSLVERSION => 3,
-            CURLOPT_SSL_CIPHER_LIST => 'SSLv3'
+            CURLOPT_SSLVERSION => 4,
+            CURLOPT_SSL_CIPHER_LIST => 'TLSv1'
 		);
 
 		if ( $this->can_followlocation && $config['follow_redirects'] ) {
